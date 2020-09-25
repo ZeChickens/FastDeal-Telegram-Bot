e8496ebd078558e0ce99074c8d1b9429a4434d0d
OnAzart <61052050+OnAzart@users.noreply.github.com>
2020-09-25 18:30:54 +0300

    add hooks

--- bot.py
+++ bot.py
@@ -12,11 +12,34 @@ from sections.redaction import Redaction
 import error_logging
 
 import logging
+import flask
 from time import sleep
 
 API_TOKEN = "1299410110:AAG90vTSRtMmKxNabMVnIYr1lDAFUetcuAU"
 
+WEBHOOK_HOST = '34.67.159.82' # external ip in Computer engine
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './url_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './url_private.key'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
 bot = TeleBot(API_TOKEN)
+
 data = Data(bot=bot)
 system = System(data=data)
 
@@ -25,6 +48,27 @@ tag = Tag(data=data)
 channel = Channel(data=data)
 order = Order(data=data)
 redaction = Redaction(data=data, order=order)
+
+app = flask.Flask(__name__)
+
+
+# Empty webserver index, return nothing, just http 200
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+# Process webhook calls
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
@@ -116,16 +160,20 @@ def oops(call, current_frame, additional_info=None):
     error_logging.send_error_info_message(bot, current_frame, additional_info=additional_info)
 
 if __name__ == "__main__":
-    logger = logging.Logger("Polling")
-    while True:
-        try:
-            bot.infinity_polling(none_stop=True)
-        except Exception as e:
-            logger.error(e)
+    # logger = logging.Logger("Polling")
+    bot.remove_webhook()
 
-            sleep(15)
+	time.sleep(1)
 
-            logging.info("Running again!")
+	# Set webhook
+	bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+					certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+	# Start flask server
+	app.run(host=WEBHOOK_LISTEN,
+			port=WEBHOOK_PORT,
+			ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+			debug=True)
 
 
 
